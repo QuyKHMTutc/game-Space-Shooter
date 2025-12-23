d01bda367cb7ddbab76b15b7c66e2d96db4516f0
guyguy <quykhmtutc@gmail.com>
2025-12-23 19:44:32 +0700

    player

---
+++ src/Player.java
@@ -0,0 +1,48 @@
+import java.awt.*;
+
+public class Player extends GameObject {
+    private boolean left, right;
+    private int speed = 8;
+    private int shootCooldown = 0;
+
+    public Player(int x, int y) {
+        super(x, y, 50, 50, Color.CYAN);
+    }
+
+    @Override
+    public void update() {
+        // Di chuyển trái
+        if (left && x > 0) {
+            x -= speed;
+        }
+        // Di chuyển phải (Sử dụng biến tĩnh WIDTH từ main class)
+        if (right && x < SpaceShooterGame.WIDTH - width) {
+            x += speed;
+        }
+
+        // Giảm thời gian chờ bắn
+        if (shootCooldown > 0) shootCooldown--;
+    }
+
+    @Override
+    public void draw(Graphics2D g) {
+        g.setColor(color);
+        // Vẽ phi thuyền hình tam giác
+        int[] xPoints = {x, x + width / 2, x + width};
+        int[] yPoints = {y + height, y, y + height};
+        g.fillPolygon(xPoints, yPoints, 3);
+
+        // Vẽ lửa động cơ
+        g.setColor(Color.ORANGE);
+        g.fillRect(x + 15, y + height, 6, 10);
+        g.fillRect(x + 29, y + height, 6, 10);
+    }
+
+    // Các phương thức điều khiển
+    public void setLeft(boolean b) { left = b; }
+    public void setRight(boolean b) { right = b; }
+
+    public boolean canShoot() { return shootCooldown == 0; }
+
+    public void resetCooldown() { shootCooldown = 15; }
+}
